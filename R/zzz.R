3c247b84c4b1cfca782594327bc8066cf831ad02
lcougnaud <laure.cougnaud@openanalytics.eu>
2020-10-26 09:07:23 +0100

    add imports utils packageDescription

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
+#' @importFrom utils packageDescription
 .onAttach <- function(libname, pkgname){
   options(error = NULL)
   packageStartupMessage(paste("\na4 version ", packageDescription("a4")$Version, 
